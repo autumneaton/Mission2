f568ea11df48a2e7c54d5d431bde6666a38d0d8e
autumneaton <autumneaton@AUTUMNEATON3476>
2024-01-22 18:03:24 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using Mission2;
+// This is so I don't need to write system in front
+// using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // This is creating an instance of the Dice class
+        Results r = new Results();
+
+        // Ask for number of rolls
+        Console.WriteLine("Welcome to the dice throwing simulator!");
+        Console.WriteLine("How many dice rolls would you like to simulate?");
+
+        // Create rolls variable
+        int numRolls = int.Parse(Console.ReadLine());
+
+        // Simulate the dice rolls and get the results array
+        int[] results = r.RollDice(numRolls);
+
+        // Print the * results
+        Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+        Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+        Console.WriteLine("Total number of rolls = " + numRolls);
+
+        for (int i = 2; i <= 12; i++)
+        {
+            // Calculate the percentage of each result
+            double percentage = (double)results[i] / numRolls * 100;
+
+            // Round the percentage to the nearest integer
+            int rounded = (int)Math.Round(percentage);
+
+            // Print the result and the corresponding number of asterisks
+            Console.Write($"{i}: ");
+            for (int j = 0; j < rounded; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+        // Thank the user and end the program
+        Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+    }
+}
---
+++ Results.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2
+{
+    class Results
+    {
+        // random number generator
+        private Random random;
+
+        public Results()
+        {
+            random = new Random();
+        }
+
+        // Method simulating the the roll of the dice
+        public int[] RollDice(int numRolls)
+        {
+            int[] results = new int[13];
+
+            // For loop for the number of rolls
+            for (int i = 0; i < numRolls; i++)
+            {
+                // Generate two random numbers between 1 and 6
+                int die1 = random.Next(1, 7);
+                int die2 = random.Next(1, 7);
+
+                int sum = die1 + die2;
+
+                // Increment the outcome array
+                results[sum]++;
+            }
+
+            // Return the outcome array
+            return results;
+        }
+    }
+}
